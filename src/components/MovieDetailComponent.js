8e9722d5b5a71f2fa324d3f337e81a04bd54ef0b
Naveen <bsa.naveenbabu@gmail.com>
2023-12-16 21:27:55 +0530

    Movie Detail Component added

--- src/components/MovieDetailComponent.js
+++ src/components/MovieDetailComponent.js
@@ -25,60 +25,114 @@ export default function MovieDetailComponent() {
                 </Col>
             </Row>
             <Row className='mt-4 mb-4'>
-                <Carousel>
-                    <Col md={3}>
-                        {/* First Column in the First Row */}
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>Card Title 1</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={3}>
-                        {/* First Column in the First Row */}
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>Card Title 1</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={3}>
-                        {/* First Column in the First Row */}
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>Card Title 1</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                    <Col md={3}>
-                        {/* First Column in the First Row */}
-                        <Card style={{ width: '18rem' }}>
-                            <Card.Img variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>Card Title 1</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of the card's content.
-                                </Card.Text>
-                                <Button variant="primary">Go somewhere</Button>
-                            </Card.Body>
-                        </Card>
-                    </Col>
-                </Carousel>
+                <Col>
+                    <Carousel>
+                        <Carousel.Item>
+                            <Row>
+                                <Col md={3}>
+                                    {/* First Column in the First Row */}
+                                    <Card style={{ width: '18rem' }}>
+                                        <Card.Img variant="top" src="holder.js/100px180" />
+                                        <Card.Body>
+                                            <Card.Title>Card Title 1</Card.Title>
+                                            <Card.Text>
+                                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                                            </Card.Text>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col md={3}>
+                                    {/* First Column in the First Row */}
+                                    <Card style={{ width: '18rem' }}>
+                                        <Card.Img variant="top" src="holder.js/100px180" />
+                                        <Card.Body>
+                                            <Card.Title>Card Title 1</Card.Title>
+                                            <Card.Text>
+                                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                                            </Card.Text>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col md={3}>
+                                    {/* First Column in the First Row */}
+                                    <Card style={{ width: '18rem' }}>
+                                        <Card.Img variant="top" src="holder.js/100px180" />
+                                        <Card.Body>
+                                            <Card.Title>Card Title 1</Card.Title>
+                                            <Card.Text>
+                                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                                            </Card.Text>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                <Col md={3}>
+                                    {/* First Column in the First Row */}
+                                    <Card style={{ width: '18rem' }}>
+                                        <Card.Img variant="top" src="holder.js/100px180" />
+                                        <Card.Body>
+                                            <Card.Title>Card Title 1</Card.Title>
+                                            <Card.Text>
+                                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                                            </Card.Text>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Carousel.Item>
+
+                        <Carousel.Item>
+                            <Col md={3}>
+                                {/* Second Column in the First Row */}
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src="holder.js/100px180" />
+                                    <Card.Body>
+                                        <Card.Title>Card Title 2</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">Go somewhere</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Carousel.Item>
+
+                        <Carousel.Item>
+                            <Col md={3}>
+                                {/* Third Column in the First Row */}
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src="holder.js/100px180" />
+                                    <Card.Body>
+                                        <Card.Title>Card Title 3</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">Go somewhere</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Carousel.Item>
+
+                        <Carousel.Item>
+                            <Col md={3}>
+                                {/* Fourth Column in the First Row */}
+                                <Card style={{ width: '18rem' }}>
+                                    <Card.Img variant="top" src="holder.js/100px180" />
+                                    <Card.Body>
+                                        <Card.Title>Card Title 4</Card.Title>
+                                        <Card.Text>
+                                            Some quick example text to build on the card title and make up the bulk of the card's content.
+                                        </Card.Text>
+                                        <Button variant="primary">Go somewhere</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        </Carousel.Item>
+                    </Carousel>
+                </Col>
             </Row>
         </div>
     );
